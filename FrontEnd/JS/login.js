6c722d7ae558ffc27021060e8e60fe04bb6c5f0e
Giraltdereve <celine.rouaire@gmail.com>
2023-02-16 10:49:57 +0100

    commentaire login

--- FrontEnd/JS/login.js
+++ FrontEnd/JS/login.js
@@ -1,44 +1,46 @@
 // -----------------------------------------------------LOGIN -----------------------------------------------------
 
-// gestion de l'événement de soumission du formulaire
-document
-  .getElementById("formulaire-login")
-  // on récupére le formulaire sur le DOM
-  .addEventListener("submit", async (event) => {
-    // écoute de l'événement de soumission du formulaire
-    event.preventDefault();
-    // l'événement par defaut de la soumission du formulaire est empéchée
-    // (evt par defaut = envoi des données au serveur pour traitement du formulaire quand on click entrée)
+// on récupére le formulaire qu'on stock dans une constante
+const form = document.getElementById("formulaire-login");
+// écoute de l'événement de soumission. Objet event contient infos événement + données formulaire soumis
+form.addEventListener("submit", async (event) => {
+  // l'événement par defaut de la soumission du formulaire est empéchée
+  event.preventDefault();
 
-    // collect des données du formulaire en utilisant l'objet "FormData" et appelant la méthode get()
-    const formData = new FormData(event.target);
-    const email = formData.get("email");
-    const password = formData.get("password");
+  // collect des données du formulaire en utilisant l'objet "FormData" et en y associant l'email et password (append)
+  const formData = new FormData();
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  formData.append("email", email.value);
+  formData.append("password", password.value);
 
-    // envoi une requête HTTP POST à l'API de connexion avec les informations d'authentification de l'architecte
-    const response = await fetch("http://localhost:5678/api/users/login", {
-      method: "POST",
-      headers: {
-        // pour indiquer le type de données : chaine de caractères (string)
-        "Content-Type": "application/json",
-      },
-      //   Ici le body qu'on envoi avec l'objet qui est convertit en json
-      body: JSON.stringify({
-        email: "sophie.bluel@test.tld",
-        password: "S0phie",
-      }),
-    });
-
-    // traite la réponse de l'API pour savoir si connexion réussie ou non
-    if (response.ok) {
-      const userInfos = await response.json();
-      console.log(userInfos);
-      // stock dans le session storage les informations d'identification de l'utilisateur (idUser et token de la session)
-      sessionStorage.setItem("user", JSON.stringify(userInfos));
-      // redirige l'utilisateur vers la page d'accueil
-      window.location.href = "./index.html";
-    } else {
-      // affiche à l'user un message d'erreur quand mdp et/ou email incorrect
-      alert("Les informations d'authentification sont incorrectes");
-    }
+  // envoi une requête HTTP POST à l'API de connexion avec les informations d'authentification de l'architecte
+  const response = await fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      // pour indiquer le type de données : chaine de caractères (string)
+      "Content-Type": "application/json",
+    },
+    //   Ici le body qu'on envoi avec l'objet qui est convertit en json
+    body: JSON.stringify({
+      email: "sophie.bluel@test.tld",
+      password: "S0phie",
+    }),
   });
+
+  // traite la réponse de l'API pour savoir si connexion réussie ou non
+  if (response.ok) {
+    // stock réponse serveur (chaîne JSON qui représente les informations de l'utilisateur) dans variable userInfos
+    const userInfos = await response.json();
+    console.log(userInfos);
+    // sessionStorage: objet JavaScript pour stocker données temporairement et localement côté client le temps session
+    // setItem prend deux arguments : une clé("user") pour identifier l'élément stocké et sa valeur(userInfos)
+    // JSON.stringify() : convertir objet JavaScript userInfos en chaîne JSON pour stockage
+    sessionStorage.setItem("user", JSON.stringify(userInfos));
+    // redirige l'utilisateur vers la page d'accueil
+    window.location.href = "./index.html";
+  } else {
+    // affiche à l'user un message d'erreur quand mdp et/ou email incorrect
+    alert("Les informations d'authentification sont incorrectes");
+  }
+});
