c3ecf474bba3a1e971172e7d6e4f8fb9b0fb1a19
Giraltdereve <celine.rouaire@gmail.com>
2023-03-16 14:10:31 +0100

    modifcation login

--- FrontEnd/JS/login.js
+++ FrontEnd/JS/login.js
@@ -1,6 +1,4 @@
 // -----------------------------------------------------LOGIN -----------------------------------------------------
-
-// on récupére le formulaire qu'on stock dans une constante
 const form = document.getElementById("formulaire-login");
 // écoute de l'événement de soumission. Objet event contient infos événement + données formulaire soumis
 form.addEventListener("submit", async (event) => {
@@ -11,42 +9,48 @@ form.addEventListener("submit", async (event) => {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  if (password !== "S0phie") {
-    alert("Mot de passe incorrect.");
-    return;
-  }
+  async function connexionUser() {
+    // envoi une requête HTTP POST à l'API de connexion avec les informations d'authentification de l'architecte
+    const response = await fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: {
+        // pour indiquer le type de données : chaine de caractères (string)
+        "Content-Type": "application/json",
+      },
+      //   Ici le body qu'on envoi avec l'objet qui est convertit en json
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
 
-  if (email !== "sophie.bluel@test.tld") {
-    alert("E-mail incorrect.");
-    return;
+    // traite la réponse de l'API pour savoir si connexion réussie ou non
+    if (response.status === 200) {
+      // stock réponse serveur qui représente les informations de l'utilisateur dans variable userInfos
+      const userInfos = await response.json();
+      sessionStorage.setItem("user", JSON.stringify(userInfos));
+      // redirige l'utilisateur vers la page d'accueil
+      window.location.href = "./index.html";
+    } else {
+      // affiche à l'user un message d'erreur quand mdp et/ou email incorrect
+      alert("Le mot de passe et/ou l'e-mail est incorrecte");
+    }
   }
-  // envoi une requête HTTP POST à l'API de connexion avec les informations d'authentification de l'architecte
-  const response = await fetch("http://localhost:5678/api/users/login", {
-    method: "POST",
-    headers: {
-      // pour indiquer le type de données : chaine de caractères (string)
-      "Content-Type": "application/json",
-    },
-    //   Ici le body qu'on envoi avec l'objet qui est convertit en json
-    body: JSON.stringify({
-      email,
-      password,
-    }),
-  });
-
-  // traite la réponse de l'API pour savoir si connexion réussie ou non
-  if (response.ok) {
-    // stock réponse serveur (chaîne JSON qui représente les informations de l'utilisateur) dans variable userInfos
-    const userInfos = await response.json();
-    console.log(userInfos);
-    // sessionStorage: objet JavaScript pour stocker données temporairement et localement côté client le temps session
-    // setItem prend deux arguments : une clé("user") pour identifier l'élément stocké et sa valeur(userInfos)
-    // JSON.stringify() : convertir objet JavaScript userInfos en chaîne JSON pour stockage
-    sessionStorage.setItem("user", JSON.stringify(userInfos));
-    // redirige l'utilisateur vers la page d'accueil
-    window.location.href = "./index.html";
-  } else {
-    // affiche à l'user un message d'erreur quand mdp et/ou email incorrect
-    alert("Les informations d'authentification sont incorrectes");
-  }
+  connexionUser();
 });
+
+// ----------- changer couleur bouton submit quand champs remplis ---------------
+function checkInputs() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (email && password.length > 0) {
+    document.getElementById("button-login").classList.add("filled");
+    document.getElementById("button-login").classList.remove("button-submit");
+  } else {
+    document.getElementById("button-login").classList.remove("filled");
+    document.getElementById("button-login").classList.add("button-submit");
+  }
+}
+
+form.addEventListener("input", checkInputs);
